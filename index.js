78b64bc3ff023f4d1c18f725208f5fa2f48466b3
Chris Hayen <chayen@gmail.com>
2015-01-21 13:34:21 -0500

    Holy shit. Mocha+q for async

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 'use strict';
-module.exports = function (str) {
-  console.log(str || 'Rainbow');
+
+var http = require('http');
+var fs = require('fs');
+var q = require('q');
+
+module.exports = {
+
+  test: function() {
+    var file = fs.createWriteStream("file.jpg");
+    var deferred = q.defer();
+
+    http.get("http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg", function(response) {
+      response.pipe(file);
+      deferred.resolve('good');
+    })
+    .on('error', function(e) {
+      deferred.reject(new Error(e.message));
+    });
+
+    return deferred.promise;
+  },
+
 };
--- test/test.js
+++ test/test.js
@@ -4,8 +4,18 @@ var assert = require('assert');
 var shapefile2mongo = require('../');
 
 describe('shapefile2mongo node module', function () {
-  it('must have at least one test', function () {
-    shapefile2mongo();
-    assert(false, 'I was too lazy to write any tests. Shame on me.');
+
+  it('must have at least one test', function (done) {
+    this.timeout(5000);
+    shapefile2mongo.test()
+    .then(function(result) {
+      assert.equal('good', result);
+      done();
+    })
+    .catch(function(err){
+      done(err);
+    });
+
   });
+
 });
